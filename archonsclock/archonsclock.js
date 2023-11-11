e093ea83e1628dffe14743af48118114a08f7971
TheArchons <progamer9138@gmail.com>
2023-11-11 15:22:31 -0800

    add battery

--- archonsclock/archonsclock.js
+++ archonsclock/archonsclock.js
@@ -2,13 +2,15 @@ require("Font8x12").add(Graphics);
 
 function draw() {
   // work out how to display the current timed
-  const dayNames = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+  const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   var d = new Date();
   var month = d.getMonth(), day = dayNames[d.getDay()], date = d.getDate();
   var h = d.getHours(), m = d.getMinutes(); s = d.getSeconds();
   var time = h + ":" + m.toString() + ":" + s.toString().padStart(2,0);
 
   var dayweek = day + " " + month + "-" + date;
+
+  var bat = E.getBattery();
 
   // Reset the state of the graphics library
   g.reset();
@@ -20,6 +22,14 @@ function draw() {
 
   g.setFont("8x12", 2);
   g.drawString(dayweek, 20, 30);
+
+  if (bat < 30) {
+    g.setColor(255, 0, 0);
+  } else {
+    g.setColor(0, 255, 0);
+  }
+
+  g.drawString(bat + "%", 20, 80);
 }
 
 // Clear the screen once, at startup
