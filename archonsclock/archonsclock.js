808042f7c17ecc492a8118dcba3b74e7c9ee5b0b
TheArchons <progamer9138@gmail.com>
2023-11-11 14:51:07 -0800

    add archonsclock

---
+++ archonsclock/archonsclock.js
@@ -0,0 +1,30 @@
+require("Font8x12").add(Graphics);
+
+function draw() {
+  // work out how to display the current timed
+  const dayNames = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+  var d = new Date();
+  var month = d.getMonth(), day = dayNames[d.getDay()], date = d.getDate();
+  var h = d.getHours(), m = d.getMinutes(); s = d.getSeconds();
+  var time = h + ":" + m.toString() + ":" + s.toString().padStart(2,0);
+
+  var dayweek = day + " " + month + "-" + date;
+
+  // Reset the state of the graphics library
+  g.reset();
+  // Clear the area where we want to draw the time
+  g.clearRect(20,30,170,170);
+  g.setFont("8x12", 3);
+  // draw the current time
+  g.drawString(time, 20, 50);
+
+  g.setFont("8x12", 2);
+  g.drawString(dayweek, 20, 30);
+}
+
+// Clear the screen once, at startup
+g.clear();
+// draw immediately at first
+draw();
+// now draw every second
+var secondInterval = setInterval(draw, 1000);
