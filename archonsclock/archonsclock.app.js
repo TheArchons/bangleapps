6db7e7466bc61bb724d09005ed6fde12e0b30393
TheArchons <progamer9138@gmail.com>
2023-11-12 08:24:47 -0800

    do not clear the widget area

--- archonsclock/archonsclock.app.js
+++ archonsclock/archonsclock.app.js
@@ -21,6 +21,44 @@ const bgsec = {
   buffer : atob("PPPPPPPPPPPPPPPPPPPPPPPP5/5/5/5/5/5/5/5/5/5/5/5/PPPPPPPPPPPPPPPPPPPPPPPP757575757575757575757575PPPPPPPPPPPPPPPPPPPPPPPP5/5/5/5/5/5/5/5/5/5/5/5/PPPPPPPPPPPPPPPPPPPPPPPP757575757575757575757575PPPPPPPPPPPPPPPPPPPPPPPP5/5/5/5/5/5/5/5/5/5/5/5/PPPPPPPPPPPPPPPPPPPPPPPP757575757575757575757575PPPPPPPPPPPPPPPPPPPPPPPP5/5/5/5/5/5/5/5/5/5/5/5/PPPPPPPPPPPPPPPPPPPPPPPP757575757575757575757575PPPPPPPPPPPPPPPPPPPPHPPP575757575/5/5/5/57575757PPPPPPPPPPPPPPPPPPPPPPPP757575757575757575757575PPPPPPPPHPPPPPPPPPPPPPPP575757575/5/5/5/57575757PPPPPPPPPPPPPPPPPPPPPPPP757575757575757575757575PPPPPPPPPPPPPPPPPPPPPPPP575757575/5/5/5757575757PPPPPPPPPPPPPPPPPPPPPPPP757575757575757575757575PPPPPPPPPPPPPPPPPPPPPPPP")
 };
 
+/* Custom version of Bangle.drawWidgets (does not clear the widget areas) Thanks to rozek */
+// copied from the rolex app
+
+Bangle.drawWidgets = function () {
+  var w = g.getWidth(), h = g.getHeight();
+
+  var pos = {
+    tl:{x:0,   y:0,    r:0, c:0}, // if r==1, we're right->left
+    tr:{x:w-1, y:0,    r:1, c:0},
+    bl:{x:0,   y:h-24, r:0, c:0},
+    br:{x:w-1, y:h-24, r:1, c:0}
+  };
+
+  if (global.WIDGETS) {
+    for (var wd of WIDGETS) {
+      var p = pos[wd.area];
+      if (!p) continue;
+
+      wd.x = p.x - p.r*wd.width;
+      wd.y = p.y;
+
+      p.x += wd.width*(1-2*p.r);
+      p.c++;
+    }
+
+    g.reset();                                 // also loads the current theme
+
+    try {
+      for (var wd of WIDGETS) {
+        g.setClipRect(wd.x,wd.y, wd.x+wd.width-1,23);
+        wd.draw(wd);
+      }
+    } catch (e) { print(e); }
+
+    g.reset();                               // clears the clipping rectangle!
+    }
+};
+
 function getTemp() {
   var weatherJson;
 
