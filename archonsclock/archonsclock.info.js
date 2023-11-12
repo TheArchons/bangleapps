52130b233a3b0fdeb6aac0d3d241bcfff7f04f64
TheArchons <progamer9138@gmail.com>
2023-11-11 20:28:38 -0800

    add clock as type

--- archonsclock/archonsclock.info.js
+++ archonsclock/archonsclock.info.js
@@ -2,5 +2,6 @@ require("Storage").write("archonsclock.info",{
   "id":"archonsclock",
   "name":"Archons Clock",
   "src":"archonsclock.app.js",
-  "icon":"archonsclock.img"
+  "icon":"archonsclock.img",
+  "type":"clock"
 });
