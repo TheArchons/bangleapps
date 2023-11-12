bcbb1030708403aee20c53b2d5773bf8128ba787
TheArchons <progamer9138@gmail.com>
2023-11-11 18:55:54 -0800

    move date + battery to a slower draw function

--- archonsclock/archonsclock.js
+++ archonsclock/archonsclock.js
@@ -1,5 +1,6 @@
 require("Font8x12").add(Graphics);
 
+const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 function drawSec() {
   var d = new Date();
   var s = d.getSeconds();
@@ -13,26 +14,34 @@ function drawSec() {
 
 function draw() {
   // work out how to display the current timed
-  const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   var d = new Date();
-  var month = d.getMonth(), day = dayNames[d.getDay()], date = d.getDate();
+
   var h = d.getHours(), m = d.getMinutes(); s = d.getSeconds();
   var time = h + ":" + m.toString() + ":" + s.toString().padStart(2,0);
 
-  var dayweek = day + " " + month + "-" + date;
-
-  var bat = E.getBattery();
-
-  // Reset the state of the graphics library
-  g.reset();
   // Clear the area where we want to draw the time
-  g.clearRect(20,30,170,170);
-  g.setFont("8x12", 3);
+  g.clearRect(0,52,170,80);
+  
   // draw the current time
   g.drawString(time, 20, 50);
+}
+
+function drawSlow() {
+  g.clear();
+  var d = new Date();
+
+  // update date
+  var month = d.getMonth() + 1, day = dayNames[d.getDay()], date = d.getDate();
+  var dayweek = day + " " + month + "-" + date;
 
   g.setFont("8x12", 2);
   g.drawString(dayweek, 20, 30);
+
+  g.setFont("8x12", 3);
+
+  // update battery
+
+  var bat = E.getBattery();
 
   if (bat < 30) {
     g.setColor(255, 0, 0);
@@ -43,12 +52,16 @@ function draw() {
   g.drawString(bat + "%", 20, 80);
 
   g.setColor(0);
-  g.setFont("8x12", 3);
+
+  draw();
 }
 
 // Clear the screen once, at startup
 g.clear();
 // draw immediately at first
+drawSlow();
 draw();
+
 // now draw every second
 var secondInterval = setInterval(drawSec, 1000);
+var slowInterval = setInterval(drawSlow, 600000);
