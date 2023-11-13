cc5d24a034f5cf38d46e49ea43039e92872b87d3
TheArchons <progamer9138@gmail.com>
2023-11-12 17:00:53 -0800

    load frames from JSON instead

--- animate/video_bitmap.py
+++ animate/video_bitmap.py
@@ -3,23 +3,21 @@ import json
 import subprocess
 
 video_dir = input("video dir: ")
+output_dir = input("output dir: ")
 
 files = [f for f in os.listdir(video_dir)]
 files.sort()
 
-images = []
-
-for file in files:
+for fileNum, file in enumerate(files):
     # convert to bitmap
     subprocess.run(["convert", os.path.join(video_dir, file), "-depth", "1", "gray:temp.raw"])
     # convert to base64
     text = subprocess.run(["base64", "--wrap=0", "temp.raw"], capture_output=True).stdout.decode()
-    # read as text
-    images.append(text)
+
+    # write to json file
+    with open(os.path.join(output_dir, f"{fileNum}.json"), "w") as f:
+        f.write(json.dumps({"data": text}))
+
 
 # remove temp files
 os.remove("temp.raw")
-
-# write as json
-with open("video.json", "w") as f:
-    json.dump(images, f)
